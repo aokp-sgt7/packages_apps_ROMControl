ea381302e818f339b64d474c8c2c70facb461502
stimpz0r <stimpz0r@kriticalcrew.org>
2012-04-22 23:46:21 +1000

    disable hide buttons / force menu button - now apart of AOKP navbar mods.

--- src/com/aokp/romcontrol/fragments/TabletTweaks.java
+++ src/com/aokp/romcontrol/fragments/TabletTweaks.java
@@ -32,11 +32,13 @@ import java.io.IOException;
 
 public class TabletTweaks extends AOKPPreferenceFragment implements OnPreferenceChangeListener 
 {
+/*
     private static final String TABLET_TWEAKS_HIDE_HOME = "tablet_tweaks_hide_home";
     private static final String TABLET_TWEAKS_HIDE_RECENT = "tablet_tweaks_hide_recent";
     private static final String TABLET_TWEAKS_HIDE_BACK = "tablet_tweaks_hide_back";
     private static final String TABLET_TWEAKS_HIDE_MENU = "tablet_tweaks_hide_menu";
     private static final String TABLET_TWEAKS_FORCE_MENU = "tablet_tweaks_force_menu";
+*/
     private static final String TABLET_TWEAKS_RIGHT_BUTTONS = "tablet_tweaks_right_buttons";
     public static final String TABLET_TWEAKS_DISABLE_HARDWARE_BUTTONS = "tablet_tweaks_disable_hardware_buttons";
     private static final String TABLET_TWEAKS_RECENT_THUMBNAILS = "tablet_tweaks_recent_thumbnails";
@@ -50,11 +52,13 @@ public class TabletTweaks extends AOKPPreferenceFragment implements OnPreference
             " > /sys/devices/platform/s3c2440-i2c.2/i2c-2/2-004a/buttons_enabled";
     public static final String BUTTONS_ENABLED_SHELL = "/system/bin/sh";
 
+/*
     CheckBoxPreference mTabletTweaksHideHome;
     CheckBoxPreference mTabletTweaksHideRecent;
     CheckBoxPreference mTabletTweaksHideBack;
     CheckBoxPreference mTabletTweaksHideMenu;
     CheckBoxPreference mTabletTweaksForceMenu;
+*/
     CheckBoxPreference mTabletTweaksRightButtons;
     CheckBoxPreference mTabletTweaksDisableHardwareButtons;
     CheckBoxPreference mTabletTweaksRecentThumbnails;
@@ -70,6 +74,7 @@ public class TabletTweaks extends AOKPPreferenceFragment implements OnPreference
         addPreferencesFromResource(R.xml.tablet_tweaks);
         PreferenceScreen prefs = getPreferenceScreen();
 
+/*
         mTabletTweaksHideBack = (CheckBoxPreference) findPreference(TABLET_TWEAKS_HIDE_BACK);
         mTabletTweaksHideBack.setChecked(Settings.System.getInt(getActivity().getContentResolver(),
                 Settings.System.HIDE_SOFT_BACK_BUTTON, 0) == 1);
@@ -85,6 +90,7 @@ public class TabletTweaks extends AOKPPreferenceFragment implements OnPreference
         mTabletTweaksForceMenu = (CheckBoxPreference) findPreference(TABLET_TWEAKS_FORCE_MENU);
         mTabletTweaksForceMenu.setChecked(Settings.System.getInt(getActivity().getContentResolver(),
                 Settings.System.FORCE_SOFT_MENU_BUTTON, 0) == 1);
+*/
         mTabletTweaksDisableHardwareButtons = (CheckBoxPreference) findPreference(TABLET_TWEAKS_DISABLE_HARDWARE_BUTTONS);
         mTabletTweaksRecentThumbnails = (CheckBoxPreference) findPreference(TABLET_TWEAKS_RECENT_THUMBNAILS);
         mTabletTweaksRecentThumbnails.setChecked(Settings.System.getInt(getActivity().getContentResolver(),
@@ -108,13 +114,16 @@ public class TabletTweaks extends AOKPPreferenceFragment implements OnPreference
             ((PreferenceGroup) findPreference("storage")).removePreference(mTabletTweaksStorageSwitch);
         }
 
+	// not working at the moment, will get this sorted out hopefully soon.
+        ((PreferenceGroup) findPreference("storage")).removePreference(mTabletTweaksStorageSwitch);
+        ((PreferenceGroup) findPreference("storage")).removePreference(mTabletTweaksStorageAutomount);
 
     }
 
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen,
             Preference preference) {
-
+/*
         if (preference == mTabletTweaksHideHome) {
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.HIDE_SOFT_HOME_BUTTON, 
@@ -141,6 +150,8 @@ public class TabletTweaks extends AOKPPreferenceFragment implements OnPreference
 		    ((CheckBoxPreference) preference).isChecked() ? 1 : 0);
             return true;
         } else if (preference == mTabletTweaksDisableHardwareButtons) {
+*/
+        if (preference == mTabletTweaksDisableHardwareButtons) {
             try {
                 String[] cmds = {BUTTONS_ENABLED_SHELL, "-c",
                         BUTTONS_ENABLED_COMMAND + (((CheckBoxPreference) preference).isChecked() ? "0" : "1") + BUTTONS_ENABLED_PATH};
